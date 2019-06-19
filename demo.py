59f7b543b28ac8286b05d1674842d558a3c1e76e
zhaofeng-shu33 <616545598@qq.com>
2019-06-19 15:54:12 +0800

    add new experiment

---
+++ demo.py
@@ -0,0 +1,51 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+from info_detection import InfoOutlierDetector
+from sklearn.covariance import EllipticEnvelope
+
+def generate_one_blob():
+    '''
+       generate training data with (300,2)
+    '''
+    n_samples = 300
+    outliers_fraction = 0.15
+    n_outliers = int(outliers_fraction * n_samples)
+    n_inliers = n_samples - n_outliers
+    blobs_params = dict(random_state=0, n_samples=n_inliers, n_features=2)
+    data, labels = make_blobs(centers=[[0, 0], [0, 0]], cluster_std=0.5,
+               **blobs_params)
+    rng = np.random.RandomState(42)
+    outlier_data = rng.uniform(low=-6, high=6, size=(n_outliers, 2))
+    total_data = np.vstack((data, outlier_data))
+    return total_data
+
+if __name__ == '__main__':
+    train_data = generate_one_blob()
+    xx, yy = np.meshgrid(np.linspace(-7, 7, 150),
+                     np.linspace(-7, 7, 150))
+
+    alg = EllipticEnvelope(contamination=0.15)
+    y_pred = alg.fit_predict(train_data)
+    # for y_pred, -1 represents outlier, 1 represents normal point
+    Z = alg.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    ic = InfoOutlierDetector(gamma=0.5) # 1/num_of_features
+    ic.fit(train_data, use_psp_i=True) # use the fastest implementation
+    predict_cat = ic.partition_num_list[-2]
+    labels = np.asarray(ic.get_category(predict_cat))
+    Z1 = ic.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z1 = Z1.reshape(xx.shape)
+
+    plt.figure(figsize=(12,6))
+    plt.subplot(1,2,1)
+    plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='black')
+    plt.scatter(train_data[y_pred==1,0], train_data[y_pred==1,1],s=5)
+    plt.scatter(train_data[y_pred==-1,0], train_data[y_pred==-1,1],s=5)
+    plt.subplot(1,2,2)
+    plt.contour(xx, yy, Z1, levels=[0.5], linewidths=2, colors='black')
+    plt.scatter(train_data[labels==0,0], train_data[labels==0,1],s=5)
+    plt.scatter(train_data[labels>0,0], train_data[labels>0,1],s=5)
+    plt.savefig('outlier_compare.eps')
+
---
+++ info_detection.py
@@ -0,0 +1,25 @@
+# outlier detecter based on info_clustering
+from info_cluster import InfoCluster
+import numpy as np
+class InfoOutlierDetector(InfoCluster):
+    def __init__(self, gamma=1):
+        # currently only rbf metric is supported
+        super().__init__(gamma=gamma)
+    def fit(self, X, **kwargs):
+        # save the training data for prediction use
+        self.data = X
+        super().fit(X, **kwargs)
+    def predict(self, point_list):
+        '''predict whether the new data is outlier or not
+        
+        Parameters
+        ----------
+        point_list: array-like, shape (n_samples, n_features)
+
+        Returns
+        -------
+        prediction_list: array-like shape (n_samples,), dtype=bool
+        '''
+        threshold = self.critical_values[-1]
+        point_list_inner = point_list.reshape((point_list.shape[0], 1, point_list.shape[1]))
+        return np.sum(np.exp(-1.0 * np.linalg.norm(self.data-point_list_inner, axis=2)/self._gamma), axis=1) >= threshold;
